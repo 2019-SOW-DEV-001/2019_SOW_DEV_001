33a3208a9ae7f4a4c6c40881facf29e1e6b5088d
2019_SOW_DEV_001 <2019_SOW_DEV_001>
2019-05-10 00:06:50 +0200

    Player X wins by filling diagonally from right

--- TicTacToe/WinningRule.swift
+++ TicTacToe/WinningRule.swift
@@ -1,5 +1,5 @@
 class WinningRule {
-    private let winningPositions : [[BoardPosition]] = [[.topLeft,.topMiddle,.topRight], [.middleLeft,.middleMiddle,.middleRight],[.bottomLeft,.bottomMiddle,.bottomRight], [.topLeft,.middleLeft,.bottomLeft], [.topMiddle,.middleMiddle,.bottomMiddle],[.topRight,.middleRight,.bottomRight], [.topLeft, .middleMiddle, .bottomRight]]
+    private let winningPositions : [[BoardPosition]] = [[.topLeft,.topMiddle,.topRight], [.middleLeft,.middleMiddle,.middleRight],[.bottomLeft,.bottomMiddle,.bottomRight], [.topLeft,.middleLeft,.bottomLeft], [.topMiddle,.middleMiddle,.bottomMiddle],[.topRight,.middleRight,.bottomRight], [.topLeft, .middleMiddle, .bottomRight], [.topRight, .middleMiddle, .bottomLeft]]
     
     func doesSatisfy(occupiedPositions: [BoardPosition]) -> Bool {
         for singleWinPossibility in winningPositions {
--- TicTacToeTests/GameTests.swift
+++ TicTacToeTests/GameTests.swift
@@ -116,6 +116,12 @@ class GameTests: XCTestCase {
         XCTAssertTrue(gameStatus.hasPlayerOWon())
     }
     
+    func test_ShouldCallPlayerXWins_When_PlayerXfillsDiagonallyFromRight() {
+        createMoves(positions: [ .topRight,.topLeft, .middleMiddle, .middleRight, .bottomLeft])
+        
+        XCTAssertTrue(gameStatus.hasPlayerXWon())
+    }
+        
     private func createMoves(positions: [BoardPosition]) {
         for position in positions {
             game.move(atPosition: position)
