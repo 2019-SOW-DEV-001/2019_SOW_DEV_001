c67701717ab09970d88d72a80d7fb31af33bc2c4
2019_SOW_DEV_001 <2019_SOW_DEV_001>
2019-05-09 23:36:18 +0200

    O wins by filling middle row

--- TicTacToeTests/GameTests.swift
+++ TicTacToeTests/GameTests.swift
@@ -50,6 +50,12 @@ class GameTests: XCTestCase {
         XCTAssertTrue(gameStatus.hasPlayerXWon())
     }
 
+    func test_ShouldCallPlayerOWins_When_PlayerOfillsCompleteMiddleRow() {
+        createMoves(positions: [.topLeft, .middleLeft, .bottomRight, .middleMiddle, .topMiddle, .middleRight])
+        
+        XCTAssertTrue(gameStatus.hasPlayerOWon())
+    }
+    
     private func createMoves(positions: [BoardPosition]) {
         for position in positions {
             game.move(atPosition: position)
