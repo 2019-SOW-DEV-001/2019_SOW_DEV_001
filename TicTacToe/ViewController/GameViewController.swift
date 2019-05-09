bf7f8e1a73dd2efcf3fca52b5c30cff6ad64cb80
2019_SOW_DEV_001 <2019_SOW_DEV_001>
2019-05-10 00:43:40 +0200

    View controller confirms to Status protocol

--- TicTacToe/ViewController/GameViewController.swift
+++ TicTacToe/ViewController/GameViewController.swift
@@ -1,11 +1,32 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
+    private var boardPresenter : GamePresenter!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        boardPresenter = GamePresenter(gameStatus: self)
     }
 
     @IBAction func occupyPosition(_ sender: UIButton) {
     }
+}
+
+extension GameViewController : GameStatus {
+    func postionSaved() {
+    }
+    
+    func positionAlreadyOccupied() {
+    }
+    
+    func playerXWins() {
+    }
+    
+    func playerOWins() {
+    }
+    
+    func gameDraw() {
+    }
+    
+    
 }
