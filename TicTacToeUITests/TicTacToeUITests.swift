6a63213962311b090674489304a9e5ed57a4201f
2019_SOW_DEV_001 <2019_SOW_DEV_001>
2019-05-10 02:07:48 +0200

    UI Test : Top row occupied by X and Alert is shown

--- TicTacToeUITests/TicTacToeUITests.swift
+++ TicTacToeUITests/TicTacToeUITests.swift
@@ -2,4 +2,20 @@ import XCTest
 
 class TicTacToeUITests: XCTestCase {
 
+    override func setUp() {
+        super.setUp()
+        XCUIApplication().launch()
+    }
+    
+    func testShouldShowAlertWithTextPlayerXWinstheGameWhenTopRowIsOccupiedByX() {
+        let app = XCUIApplication()
+        app.buttons["leftTop"].tap()
+        app.buttons["middleLeft"].tap()
+        app.buttons["middleTop"].tap()
+        app.buttons["middleMiddle"].tap()
+        app.buttons["rightTop"].tap()
+        XCTAssertTrue(app.alerts.staticTexts["Player X Wins the Game"].exists)
+    }
+    
+    
 }
