3fdc73b8ce51593d2ec9fda6e8484b42b82dab95
2019_SOW_DEV_001 <2019_SOW_DEV_001>
2019-05-10 02:23:35 +0200

    Match Draw scenario is UI tested

--- TicTacToeUITests/TicTacToeUITests.swift
+++ TicTacToeUITests/TicTacToeUITests.swift
@@ -28,4 +28,18 @@ class TicTacToeUITests: XCTestCase {
         XCTAssertTrue(app.alerts.staticTexts["Player X Wins the Game"].exists)
     }
     
+    func testShouldShowMatchDrawWhenAllPositionsAreOccupied() {
+        let app = XCUIApplication()
+        app.buttons["leftTop"].tap()
+        app.buttons["middleLeft"].tap()
+        app.buttons["middleTop"].tap()
+        app.buttons["rightTop"].tap()
+        app.buttons["middleMiddle"].tap()
+        app.buttons["bottomMiddle"].tap()
+        app.buttons["middleRight"].tap()
+        app.buttons["bottomRight"].tap()
+        app/*@START_MENU_TOKEN@*/.buttons["bottomLeft"]/*[[".buttons[\"-\"]",".buttons[\"bottomLeft\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCTAssertTrue(app.alerts.staticTexts["Match Draw"].exists)
+    }
+    
 }
